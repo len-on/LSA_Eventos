ff935d398bb523d7c88f7301c6b046021bc21bfe
len-on <lenon.assalin@gmail.com>
2023-06-03 14:56:18 -0300

    implementação participanteinscricao

--- back/src/main/java/br/com/api/back/modelo/UsuarioModelo.java
+++ back/src/main/java/br/com/api/back/modelo/UsuarioModelo.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,10 +23,20 @@ public class UsuarioModelo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_usuario;
+
+    @NotNull
     private String nome_usuario;
+
+    @NotNull
     private String senha_usuario;
+
+    @NotNull
     private String CPF_usuario;
+
+    @NotNull
     private String email_usuario;
+
+    @NotNull
     private Date data_nascimento_usuario;
     
 }
